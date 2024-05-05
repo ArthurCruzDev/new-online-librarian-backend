f203f2092374a612fe71ad0b096b8993458bd7d4
Arthur Rodrigues Cruz <developer.arthur.cruz@gmail.com>
2024-05-05 16:01:04 -0300

    FIX[Healthcheck]: Fixes healthcheck endpoint and tests

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,10 @@
+use std::net::TcpListener;
+
 use new_online_librarian_backend::run;
 
 #[tokio::main]
 async fn main() -> Result<(), std::io::Error> {
-    run()
+    let listener = TcpListener::bind("127.0.0.1:8000")
+    .expect("Failed to bind address.");
+    run(listener)?.await
 }
