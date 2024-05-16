944365c6201bdb51915b4a9d8e950c473ae74266
Arthur Rodrigues Cruz <developer.arthur.cruz@gmail.com>
2024-05-16 18:11:11 -0300

    FEAT[User Module]: Refactors UserRepository trait and creates a test implementation on the controller

--- src/modules/users/infra/repositories/user_repository.rs
+++ src/modules/users/infra/repositories/user_repository.rs
@@ -1,9 +1,11 @@
+use std::future::Future;
+
 use crate::modules::users::domain::entities::user::User;
 use sqlx::Error;
 
 pub trait UserRepository {
-    async fn save(&self, user: &User) -> Result<User, Error>;
-    async fn find_by_id(&self, id: u64) -> Result<User, Error>;
-    async fn find_by_email(&self, email: &str) -> Result<User, Error>;
-    async fn delete_by_id(&self, id: u64) -> Result<(), Error>;
+    fn save(&self, user: &User) -> impl Future<Output = Result<User, Error>> + Send;
+    fn find_by_id(&self, id: u64) -> impl Future<Output = Result<User, Error>> + Send;
+    fn find_by_email(&self, email: &str) -> impl Future<Output = Result<User, Error>> + Send;
+    fn delete_by_id(&self, id: u64) -> impl Future<Output = Result<(), Error>> + Send;
 }
