54db983f90e98dd8d0004796f435888d29ced61a
Arthur Cruz <developer.arthur.cruz@gmail.com>
2024-05-30 15:01:35 -0300

    REFACT[UserModule]: Refactored so conversion from entity to dto is handled by a mapper

--- src/modules/users/infra/controllers/user_controller_v1.rs
+++ src/modules/users/infra/controllers/user_controller_v1.rs
@@ -1,7 +1,11 @@
-use crate::modules::users::{
-    domain::dtos::create_user_dto::CreateUserDto,
-    infra::repositories::user_repository_mysql::UserRepositoryMySQL,
-    usecases::v1::create_user::CreateUserUseCaseV1,
+use crate::modules::users::domain::mappers::user_mapper;
+use crate::modules::{
+    shared::errors::APIError,
+    users::{
+        domain::{dtos::create_user_dto::CreateUserDto, entities::user::User},
+        infra::repositories::user_repository_mysql::UserRepositoryMySQL,
+        usecases::v1::create_user::CreateUserUseCaseV1,
+    },
 };
 use actix_web::{post, web, HttpResponse, Scope};
 
@@ -22,11 +26,13 @@ async fn create_user(
     user_controller: web::Data<UserControllerV1>,
     create_user_dto: web::Json<CreateUserDto>,
 ) -> HttpResponse {
-    match user_controller
-        .create_user_usecase
-        .create_user(create_user_dto.0)
-        .await
-    {
+    let user = match User::try_from(create_user_dto.0) {
+        Ok(converted_user) => converted_user,
+        Err(e) => {
+            return HttpResponse::from(APIError::DetailedAPIError(e));
+        }
+    };
+    match user_controller.create_user_usecase.create_user(user).await {
         Ok(user) => match user.id {
             Some(id) => HttpResponse::Created().body(format!("{}", id)),
             None => HttpResponse::InternalServerError()
--- src/modules/users/usecases/v1/create_user.rs
+++ src/modules/users/usecases/v1/create_user.rs
@@ -1,25 +1,14 @@
-use argon2::{
-    password_hash::{rand_core::OsRng, PasswordHasher, SaltString},
-    Argon2,
-};
-use email_address::EmailAddress;
-use regex::Regex;
-use sha2::{Digest, Sha256};
-use sqlx::types::uuid::fmt::Simple;
-use std::{collections::HashMap, sync::Arc};
+use std::sync::Arc;
 
 use crate::modules::{
-    shared::errors::{
-        detailed_api_error::DetailedAPIError, simple_api_error::SimpleAPIError, APIError,
-    },
+    shared::errors::{simple_api_error::SimpleAPIError, APIError},
     users::{
-        domain::{dtos::create_user_dto::CreateUserDto, entities::user::User},
+        domain::entities::user::User,
         infra::repositories::{
             user_repository::UserRepository, user_repository_mysql::UserRepositoryMySQL,
         },
     },
 };
-use rand::Rng;
 
 pub struct CreateUserUseCaseV1<T>
 where
@@ -35,15 +24,12 @@ impl CreateUserUseCaseV1<UserRepositoryMySQL> {
         }
     }
 
-    pub async fn create_user(&self, create_user_dto: CreateUserDto) -> Result<User, APIError> {
-        let user = match User::try_from(create_user_dto) {
-            Ok(converted_user) => converted_user,
-            Err(e) => {
-                return Err(APIError::DetailedAPIError(e));
-            }
-        };
-
-        match self.user_repository.find_by_email(&user.email).await {
+    pub async fn create_user(&self, user_to_be_created: User) -> Result<User, APIError> {
+        match self
+            .user_repository
+            .find_by_email(&user_to_be_created.email)
+            .await
+        {
             Ok(duplicated_email_user) => {
                 if duplicated_email_user.is_some() {
                     return Err(APIError::SimpleAPIError(SimpleAPIError::new(
@@ -60,7 +46,7 @@ impl CreateUserUseCaseV1<UserRepositoryMySQL> {
             }
         }
 
-        match self.user_repository.save(&user).await {
+        match self.user_repository.save(&user_to_be_created).await {
             Ok(t) => match t {
                 Some(returned_user) => Ok(returned_user),
                 None => Err(APIError::SimpleAPIError(SimpleAPIError::new(
@@ -73,150 +59,5 @@ impl CreateUserUseCaseV1<UserRepositoryMySQL> {
                 500,
             ))),
         }
-    }
-}
-
-impl TryFrom<CreateUserDto> for User {
-    type Error = DetailedAPIError;
-
-    fn try_from(dto: CreateUserDto) -> Result<Self, Self::Error> {
-        let mut user = User::default();
-        let mut errors = false;
-        let mut validations: HashMap<String, String> = HashMap::default();
-        match dto.name {
-            Some(name) => {
-                let candidate_name = name.trim();
-                if candidate_name.is_empty() {
-                    validations.insert(
-                        "name".to_string(),
-                        "Full name must not be empty".to_string(),
-                    );
-                    errors = true;
-                }
-                if candidate_name.len() <= 2 || candidate_name.split_whitespace().count() <= 1 {
-                    validations
-                        .insert("name".to_string(), "Full name must be informed".to_string());
-                    errors = true;
-                }
-                user.name = candidate_name.to_string();
-            }
-            None => {
-                validations.insert("name".to_string(), "Name not informed".to_string());
-                errors = true;
-            }
-        }
-
-        match dto.email {
-            Some(email) => {
-                let candidate_email = email.trim();
-                if candidate_email.is_empty() {
-                    validations.insert("email".to_string(), "Email must not be empty".to_string());
-                    errors = true;
-                }
-                if EmailAddress::is_valid(candidate_email) {
-                    user.email = candidate_email.to_string();
-                } else {
-                    validations.insert(
-                        "email".to_string(),
-                        "informed email is not valid".to_string(),
-                    );
-                    errors = true;
-                }
-            }
-            None => {
-                validations.insert("email".to_string(), "Email not informed".to_string());
-                errors = true;
-            }
-        }
-
-        match dto.password {
-            Some(password) => {
-                let candidate_password = password.trim();
-
-                if candidate_password.is_empty() {
-                    validations.insert(
-                        "password".to_string(),
-                        "Password must not be empty".to_string(),
-                    );
-                    errors = true;
-                }
-
-                if candidate_password.len() < 8 {
-                    validations.insert(
-                        "password".to_string(),
-                        "Password must have at least 8 characters".to_string(),
-                    );
-                    errors = true;
-                }
-
-                if candidate_password.matches(char::is_uppercase).count() < 1 {
-                    validations.insert(
-                        "password".to_string(),
-                        "Password must have at least one uppercase character".to_string(),
-                    );
-                    errors = true;
-                }
-
-                if candidate_password.matches(char::is_numeric).count() < 1 {
-                    validations.insert(
-                        "password".to_string(),
-                        "Password must have at least one number".to_string(),
-                    );
-                    errors = true;
-                }
-
-                match Regex::new(r"[^\ssa-zA-Z0-9]") {
-                    Ok(reg) => {
-                        if !reg.is_match(candidate_password) {
-                            validations.insert(
-                                "password".to_string(),
-                                "Password must have at least one special character".to_string(),
-                            );
-                            errors = true;
-                        }
-                    }
-                    Err(error) => {
-                        return Err(DetailedAPIError {
-                            msg: error.to_string(),
-                            code: 500,
-                            field_validations: None,
-                        });
-                    }
-                }
-
-                let salt = SaltString::generate(&mut OsRng);
-                let argon2 = Argon2::default();
-                match argon2.hash_password(candidate_password.as_bytes(), &salt) {
-                    Ok(hashed_password) => {
-                        user.password = hashed_password.to_string();
-                    }
-                    Err(e) => {
-                        return Err(DetailedAPIError {
-                            msg: e.to_string(),
-                            code: 500,
-                            field_validations: None,
-                        });
-                    }
-                }
-            }
-            None => {
-                validations.insert("passoword".to_string(), "Password not informed".to_string());
-                errors = true;
-            }
-        }
-
-        let mut hasher = Sha256::new();
-        let mut rng = rand::thread_rng();
-        hasher.update(rng.gen::<u64>().to_ne_bytes());
-        user.email_token = Some(format!("{:x}", hasher.finalize()));
-
-        if errors {
-            return Err(DetailedAPIError {
-                msg: "Request contains invalid data".to_string(),
-                code: 400,
-                field_validations: Some(validations),
-            });
-        }
-        Ok(user)
     }
 }
